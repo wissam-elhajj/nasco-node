6f5df76ba0ff490a460c89e9914e8c4696a1d434
Wissam Al Hajj <wissam.alhajj@nascodigital.com>
2024-09-06 09:31:51 +0300

    third commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
